5a5ac97dfb6f8c5e80279cd92469a697de98b92e
himanshu <himanshu11199@gmail.com>
2025-03-06 14:44:08 +0530

    first commit

---
+++ dom/dom.js
@@ -0,0 +1,31 @@
+// fetching html element 
+
+// const heading = document.querySelector("h1"); 
+    //   first occurance of element that satisfies selector
+
+// const heading = document.querySelectorAll("h1");
+    // all the occurances of element that satisfies the selector
+    
+// const heading = document.getElementById("3") 
+    // element with same id that we passed as an argument - single element
+
+// const heading = document.getElementsByClassName("bca");
+    // multiple element - array like structure
+
+// const heading = document.getElementsByTagName("h1")
+    // multiple element - array like structure
+
+
+// console.log(heading)
+
+// changing text of element
+
+// heading.innerText = "hello Chitkara"
+
+
+
+// div task
+
+// const abc = document.getElementById("container");
+// abc.innerText = "<h1>Hello World</h1>"
+// abc.innerHTML = "<button>Hello World</button>"
---
+++ pattern.js
@@ -0,0 +1,21 @@
+// let row = 5;
+// let str = ""
+//         for(let i = 1; i<=row; i++){
+//             str = ""
+//             for(let j = 1; j<=i; j++){
+//                 str = str + "*"
+//             }
+//             console.log(str)
+//         }
+
+
+
+var a;
+console.log(a)
+a = 10;
+
+function abc(){
+    
+}
+abc ()
+
