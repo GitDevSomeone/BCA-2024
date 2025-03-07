419fb113af86dcf40c2c16a24d4d151300a0680e
himanshu <himanshu11199@gmail.com>
2025-03-07 16:09:20 +0530

    'events'

--- dom/dom.js
+++ dom/dom.js
@@ -29,3 +29,34 @@
 // const abc = document.getElementById("container");
 // abc.innerText = "<h1>Hello World</h1>"
 // abc.innerHTML = "<button>Hello World</button>"
+
+// const heading = document.getElementById("head");
+// console.log(heading.style)
+
+// heading.addEventListener("click", function(){
+//     heading.innerText = "hello BCA"
+//     heading.style.color = "white";
+//     heading.style.backgroundColor = "black"
+
+// })
+
+const input = document.getElementById("inp");
+
+// input.addEventListener("keypress", function(){
+//     // console.log('event triggered')
+//     console.log(input.value)
+// })
+
+
+// const para = document.getElementById('real')
+
+// input.addEventListener("input", function(){
+//     // console.log('event triggered')
+//     // console.log(input.value)
+//     para.innerText = input.value;
+// })
+
+
+input.addEventListener("blur", function(){
+   console.log('focus triggered')
+})
