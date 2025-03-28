438dba5623da4de920d019ad39729089e76e6781
himanshu <himanshu11199@gmail.com>
2025-03-28 15:31:11 +0530

    completed

--- Todo List/index(withlocalstorage).js
+++ Todo List/index(withlocalstorage).js
@@ -12,22 +12,44 @@ if(dataFromLocalStorage != null){
 
 
 taskList.forEach(function(value, index, array){
-    const li = document.createElement('li');
-    li.innerText = value;
-
-    listContainer.appendChild(li)
+    createUi(value)
 })
 
 addButton.addEventListener('click',()=>{
     let inputFieldValue = inputField.value;
-    taskList.push(inputFieldValue)
+    let taskobject = {
+        id: Date.now(),
+        name: inputFieldValue,
+    }
+    taskList.push(taskobject)
 
     localStorage.setItem("tasks", JSON.stringify(taskList))
+    createUi(taskobject)
+})
 
 
+function createUi(obj){
+    // console.log(obj)
     const li = document.createElement('li');
-    li.innerText = inputFieldValue;
+    li.setAttribute("id", obj.id)
+
+    const span = document.createElement('span');
+    span.innerText = obj.name;
+    li.appendChild(span)
+
+    const button = document.createElement('button');
+    button.innerText = "X";
+    button.addEventListener('click', ()=>{
+        taskList = taskList.filter(function(value, index, array){
+            if(value.id != li.id){
+                return true;
+            }
+        })
+        localStorage.setItem("tasks", JSON.stringify(taskList))
+        li.remove();
+    })
+    li.appendChild(button);
+
 
     listContainer.appendChild(li)
-
-})
+}
